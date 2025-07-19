e581ad7f2d5a248916b55d50d4fd5946cf018416
Koo Kuu <vinhtrungngo1907@gmail.com>
2025-07-19 14:56:18 +0700

    [update]-upgrade

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -10,8 +10,10 @@ const Dialog = DialogPrimitive.Root;
 
 const DialogTrigger = DialogPrimitive.Trigger;
 
-const DialogPortal = ({ className, ...props }: DialogPrimitive.DialogPortalProps) => (
-    <DialogPrimitive.Portal className={cn(className)} {...props} />
+const DialogPortal = ({ ...props }: DialogPrimitive.DialogPortalProps) => (
+    <DialogPrimitive.Portal
+        // className={cn(className)} 
+        {...props} />
 );
 DialogPortal.displayName = DialogPrimitive.Portal.displayName;
 
