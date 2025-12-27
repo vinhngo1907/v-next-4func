56b17d11a7d779ef65b31671be03393b383cb0cb
vinhngo1907 <vinhtrungngo1907@gmail.com>
2025-12-27 16:43:38 +0700

    fix(develop): update layout with next version 16

--- app/(dashboard)/u/[username]/layout.tsx
+++ app/(dashboard)/u/[username]/layout.tsx
@@ -1,11 +1,8 @@
 import React from "react";
 import { redirect } from "next/navigation";
-import { Metadata } from "next";
+import type { Metadata } from "next";
 
 import { getSelfByUsername } from "@/lib/auth-service";
-
-// import { Navbar } from "./_components/navbar";
-// import { Sidebar } from "./_components/sidebar";
 import { Container } from "./_components/container";
 
 export const metadata: Metadata = {
@@ -14,22 +11,19 @@ export const metadata: Metadata = {
 
 export default async function CreatorLayout({
   children,
-  params: { username },
+  params,
 }: {
   children: React.ReactNode;
-  params: { username: string };
+  params: Promise<{ username: string }>;
 }) {
-  const self = await getSelfByUsername(username);
+  const { username } = await params;
 
+  const self = await getSelfByUsername(username);
   if (!self) redirect("/");
 
   return (
-    <>
-      {/* <Navbar /> */}
-      <div className="flex h-full pt-20">
-        {/* <Sidebar /> */}
-        <Container>{children}</Container>
-      </div>
-    </>
+    <div className="flex h-full pt-20">
+      <Container>{children}</Container>
+    </div>
   );
 }
